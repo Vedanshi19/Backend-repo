ebcc974568eba98391f07ce1d9f7332404215739
Vedanshi19 <132483423+Vedanshi19@users.noreply.github.com>
2024-03-09 09:19:16 +0000

    custom api responce and error handling

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 //nodemon starts server as soon as your file gets saved
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+const app = express()
+
+export {app}
